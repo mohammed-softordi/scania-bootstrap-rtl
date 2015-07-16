ac7a6a96f6417f0a412484a2da2dfa1b532aed62
unknown <MLEBG7@RD0037530.global.scd.scania.com>
2015-07-16 15:30:20 +0200

    scania-bootstrap-rtl.js

---
+++ scania-bootstrap-rtl.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	var elementWithFontSize = getElementWithFontSize(),
+       	fontSizeProp = elementWithFontSize.css('font-size');
+    
+    elementWithFontSize.css('font-size', parseInt(fontSizeProp) + 2 + 'px');  
+          
+    function getElementWithFontSize() {
+    	var elementList = $('body').find('*');
+       	return $('.bootstrap-rtl').find('*').each(function(){
+       		if($(this).css('font-size') !== ''){
+				return $(this);
+			}
+       	});
+    }   
+});
