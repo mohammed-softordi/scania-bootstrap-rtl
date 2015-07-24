0c169ba8e99cfcd53c7678e3f9dd4741c6ab94c0
Mohammed Ayowa <mohammed.ayowa@scania.com>
2015-07-24 11:17:32 +0200

    Added a service for retrieving browser langugage settings

--- scania-bootstrap-rtl.js
+++ scania-bootstrap-rtl.js
@@ -9,7 +9,6 @@
 (function () {
     'use strict';
 
-
     /**
      * @ngdoc module
      * @name scania.bootstrap.rtl
@@ -17,7 +16,10 @@
      * @description
      * Scania lightbox module including templates
      */
-    angular.module('scania.bootstrap.rtl', ['ngLocalize', 'door3.css']).directive('scBootstrapRtl', ['$window', '$css', 'locale', scBootstrapRtl]);
+    angular.module('scania.bootstrap.rtl', ['ngLocalize', 'door3.css']);
+
+    angular.module('scania.bootstrap.rtl').directive('scBootstrapRtl', ['$css', 'browserLocaleSettings', scBootstrapRtl]);
+    angular.module('scania.bootstrap.rtl').service('browserLocaleSettings', ['$window', 'locale', browserLocaleSettings]);
     /**
      * @ngdoc directive
      * @name scBootstrapRtl
@@ -33,53 +35,15 @@
      *
      */
 
-    function scBootstrapRtl($window, $css, locale) {
+    function scBootstrapRtl($css, browserLocaleSettings) {
         return {
-            restrict: 'C',
-            scope: {
-                ngModel: '='
-            },
+            restrict: 'AC',
             controllerAs: 'bootstrapRtl',
             controller: function ($scope, $attrs) {
                 var self = this;
-                var defaultSupportedLanguages = ['ar-AR', 'sv-SE', 'en-GB'];
-                var defaultLanguage = 'sv-SE';
-                self.supportedLanguage = $scope.ngModel || defaultSupportedLanguages;
 
-                if(_.startsWith(locale.getLocale(), 'ar') || _.startsWith( getFirstBrowserLanguage(), 'ar')) {
+                if(browserLocaleSettings.isRTL()) {
                     applyRTL();
-                }
-
-                function getFirstBrowserLanguage () {
-                    var nav = $window.navigator,
-                        browserLanguagePropertyKeys = ['language', 'browserLanguage', 'systemLanguage', 'userLanguage'],
-                        i,
-                        language;
-
-                    // support for HTML 5.1 "navigator.languages"
-                    if (_.isArray(nav.languages)) {
-                        for (i = 0; i < nav.languages.length; i++) {
-                            language = nav.languages[i];
-                            return getLanguageSupported(language);
-                        }
-                    }
-
-                    // support for other well known properties in browsers
-                    for (i = 0; i < browserLanguagePropertyKeys.length; i++) {
-                        language = nav[browserLanguagePropertyKeys[i]];
-                        return getLanguageSupported(language);
-                    }
-
-                    return null;
-                }
-
-                function getLanguageSupported (language) {
-                    if (language && language.length) {
-                        var languageIndex = _.findIndex(self.supportedLanguage, function (languageSuppported) {
-                            return _.startsWith(languageSuppported, language);
-                        });
-                        return languageIndex !== -1 ? self.supportedLanguage[languageIndex] : defaultLanguage;
-                    }
                 }
 
                 function applyRTL () {
@@ -93,7 +57,7 @@
 
                     function getElementWithFontSize() {
                         var elementList = $('body').find('*');
-                        return $('.sc-bootstrap-rtl').find('*').each(function(){
+                        return $('.sc-bootstrap-rtl').find('*').not("[class^='icon'], [class^='btn'], [class^='fa'], h1, h2, h3, h4, h5, h6").each(function(){
                             if($(this).css('font-size') !== ''){
                                 return $(this);
                             }
@@ -102,5 +66,68 @@
                 }
             }
         }
+    }
+
+    /**
+     * @ngdoc service
+     * @name browserLocaleSettings
+     * @module scania.bootstrap.rtl
+     *
+     * @description
+     * browserLocaleSettings provides browser settings like language
+     *
+     * @param locale
+     * @returns {{service: Object}}
+     *
+     */
+
+    function browserLocaleSettings($window, locale) {
+        var service = {
+            init: _init,
+            getFirstBrowserLanguage: _getFirstBrowserLanguage,
+            isRTL: _isRTL
+        };
+        return service;
+
+        function _init (supportedLanguages, defaultLanguage) {
+            service.supportedLanguages = supportedLanguages;
+            service.defaultLanguage = defaultLanguage;
+        }
+
+        function _getFirstBrowserLanguage () {
+            var nav = $window.navigator,
+                browserLanguagePropertyKeys = ['language', 'browserLanguage', 'systemLanguage', 'userLanguage'],
+                i,
+                language;
+
+            // support for HTML 5.1 "navigator.languages"
+            if (_.isArray(nav.languages)) {
+                _.each(nav.languages, function(){
+                    return getLanguageSupported(nav.languages[i]);
+                });
+            }
+            // support for other well known properties in browsers
+            _.each(browserLanguagePropertyKeys, function(key){
+                return getLanguageSupported(nav[key]);
+            });
+
+            return null;
+        }
+
+        function getLanguageSupported (language) {
+            if (language && language.length) {
+                var languageIndex = _.findIndex(service.supportedLanguages, function (languageSuppported) {
+                    return _.startsWith(languageSuppported, language);
+                });
+                return languageIndex !== -1 ? service.supportedLanguages[languageIndex] : service.defaultLanguage;
+            }
+
+        }
+
+        function _isRTL () {
+            var currentLocale = locale.getLocale() || _getFirstBrowserLanguage();
+            return _.startsWith(currentLocale, 'ar');
+        }
+
     }
 })();
