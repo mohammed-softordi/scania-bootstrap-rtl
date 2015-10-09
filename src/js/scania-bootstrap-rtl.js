184a7233fb25b095b43162fd4a625119dea96849
Mohammed Ayowa <mohammed.ayowa@scania.com>
2015-10-09 14:48:43 +0200

    Releasing symbole support

--- src/js/scania-bootstrap-rtl.js
+++ src/js/scania-bootstrap-rtl.js
@@ -18,7 +18,9 @@
      */
     angular.module('scania.bootstrap.rtl', ['ngLocalize', 'door3.css']);
 
-    angular.module('scania.bootstrap.rtl').directive('scBootstrapRtl', ['$css', 'browserLocaleSettings', scBootstrapRtl]);
+    angular.module('scania.bootstrap.rtl').directive('scBootstrapRtl', ['$css', 'browserLocaleSettings', '$compile', '$timeout', scBootstrapRtl]);
+    angular.module('scania.bootstrap.rtl').directive('rtlSymbol', ['$compile', '$timeout', 'browserLocaleSettings', rtlSymbol]);
+
     angular.module('scania.bootstrap.rtl').service('browserLocaleSettings', ['$window', 'locale', browserLocaleSettings]);
     /**
      * @ngdoc directive
@@ -39,14 +41,14 @@
         return {
             restrict: 'AC',
             controllerAs: 'bootstrapRtl',
-            controller: function ($scope, $attrs) {
+            controller: function ($scope, $attrs, $rootScope) {
                 var self = this;
 
-                if(browserLocaleSettings.isRTL()) {
+                if (browserLocaleSettings.isRTL()) {
                     applyRTL();
                 }
 
-                function applyRTL () {
+                function applyRTL() {
                     $css.add('/bower_components/bootstrap-rtl/bootstrap-rtl.css');
                     $css.add('/bower_components/bootstrap-rtl/scania-font-awesome-rtl.css');
                     $css.add('/bower_components/bootstrap-rtl/scania-bootstrap-rtl.css');
@@ -58,12 +60,43 @@
 
                     function getElementWithFontSize() {
                         var elementList = $('body').find('*');
-                        return $('.sc-bootstrap-rtl').find('*').not("[class^='icon'], [class^='btn'], [class^='fa'], h1, h2, h3, h4, h5, h6").each(function(){
-                            if($(this).css('font-size') !== ''){
+                        return $('.sc-bootstrap-rtl').find('*').not("[class^='icon'], [class^='btn'], [class^='fa'], h1, h2, h3, h4, h5, h6").each(function () {
+                            if ($(this).css('font-size') !== '') {
                                 return $(this);
                             }
                         });
                     }
+                }
+            }
+        }
+    }
+
+    function rtlSymbol($compile, $timeout, browserLocaleSettings) {
+        return {
+            require: '^scBootstrapRtl',
+            restrict: 'C',
+            compile: function (tElement, tAttrs, transclude) {
+                return function postLink(scope, iElement, iAttrs, controller) {
+                    if (!browserLocaleSettings.isRTL())return;
+                    var rtlSymbols = ['%', '°F', '°C', '°'];
+                    var ltrSymbols = ['±'];
+
+                    $timeout(function(){
+                        moveSymbol(rtlSymbols, iElement, true);
+                        moveSymbol(ltrSymbols, iElement);
+                    })
+
+                };
+                function moveSymbol(symbols, iElement, rtl){
+                    var symbol = _.find(symbols, function (symbol) {
+                        return iElement.text().indexOf(symbol) !== -1;
+                    });
+
+                    if(!symbol) return;
+
+                    var value = iElement.text().replace(symbol,'');
+                    var result = (rtl) ? symbol + value : value + symbol;
+                    iElement.text(result);
                 }
             }
         }
@@ -89,18 +122,18 @@
             isRTL: _isRTL
         };
         service.defaultLanguage = 'sv-SE';
-        service.supportedRTLLanguages = ['ar-AE', 'ur-PA', 'eb-IS', 'per-IR'];
-        service.supportedLanguages = ['ar-AE', 'sv-SE', 'en-GB'];
+        service.supportedRTLLanguages = ['ar-AE', 'ur-PA', 'eb-IS', 'per-IR'];
+        service.supportedLanguages = ['ar-AE', 'sv-SE', 'en-GB'];
 
         return service;
 
-        function _init (supportedLanguages, defaultLanguage, rtlLanguages) {
+        function _init(supportedLanguages, defaultLanguage, rtlLanguages) {
             service.defaultLanguage = defaultLanguage;
             service.supportedRTLLanguages = rtlLanguages;
             service.supportedLanguages = supportedLanguages;
         }
 
-        function _getFirstBrowserLanguage () {
+        function _getFirstBrowserLanguage() {
             var nav = $window.navigator,
                 browserLanguagePropertyKeys = ['language', 'browserLanguage', 'systemLanguage', 'userLanguage'],
                 i,
@@ -110,19 +143,20 @@
             if (_.isArray(nav.languages)) {
                 for (i = 0; i < nav.languages.length; i++) {
                     var language = getLanguageSupported(nav.languages[i]);
-                    if(language) return language;
+                    if (language) return language;
                 }
             }
             // support for other well known properties in browsers
-            for (i = 0; i < browserLanguagePropertyKeys.length; i++){
+            for (i = 0; i < browserLanguagePropertyKeys.length; i++) {
                 var language = getLanguageSupported(nav[browserLanguagePropertyKeys[i]]);
-                if(language) return language;
-            };
+                if (language) return language;
+            }
+            ;
 
             return null;
         }
 
-        function getLanguageSupported (language) {
+        function getLanguageSupported(language) {
             if (language && language.length) {
                 var languageIndex = _.findIndex(service.supportedLanguages, function (languageSuppported) {
                     return _.startsWith(languageSuppported, language);
@@ -132,7 +166,7 @@
 
         }
 
-        function _isRTL () {
+        function _isRTL() {
             var currentLocale = locale.getLocale() || service.getFirstBrowserLanguage();
             return _.contains(service.supportedRTLLanguages, currentLocale);
         }
